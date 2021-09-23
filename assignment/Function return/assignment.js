94ebd5edcd5e4ea249df8da8c62ea4d23bb439c3
sahiljamod <sahiljamod123@gmail.com>
2021-09-23 13:05:13 +0530

    variale & function assignment

---
+++ assignment/Function return/assignment.js
@@ -0,0 +1,32 @@
+function sum(x,y){
+    return x + y;
+}
+let total = sum(8,10);
+
+console.log('8 + 10 = ' + total);
+
+
+
+function sub(num1,num2){
+    return num1 - num2;
+}
+let substracttion = sub(55,10);
+console.log('55 and 10 sub is ' + substracttion);
+
+
+function multiplication(number1,number2){
+
+    return number1 * number2;
+}
+
+let multi = multiplication(10,10);
+console.log('10 * 10 = ' + multi);
+
+
+
+function div(div1,div2){
+return div1 / div2;
+}
+let addition = div(500,3);
+console.log('500 / 3  = '  + addition);
+
---
+++ assignment/Variables/variables.js
@@ -0,0 +1,21 @@
+// $(window).load(function () {
+
+   
+// });
+
+var person = " Sahil jamod Bharatbhai";
+address = "Dholagam,bhavnagar";
+state = "Gujrat";
+email = "sahiljamod000@gamil.com";
+password = "123456789";
+weight = 50;
+height = 165;
+religion = "Hindu";
+document.getElementById("name").value = person ;
+document.getElementById("add").value = address;
+document.getElementById("st").value = state;
+document.getElementById("emi").value = email;
+document.getElementById("wg").value = weight  ;
+document.getElementById("hg").value = height ;
+document.getElementById("pass").value = password;
+document.getElementById("rg").value = religion;
